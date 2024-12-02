0668068b0f5ecbb852d0ba47b54f21d7bec61be9
kevkevUWU <lik777@newschool.edu>
2024-12-01 23:44:11 -0500

    Final edit

---
+++ entries/entry12/script.js
@@ -0,0 +1,34 @@
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 4,
+    slideShadows: true
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  keyboard: {
+    enabled: true
+  },
+  mousewheel: {
+    thresholdDelta: 70
+  },
+  breakpoints: {
+    560: {
+      slidesPerView: 2.5
+    },
+    768: {
+      slidesPerView: 3
+    },
+    1024: {
+      slidesPerView: 3
+    }
+  }
+});
