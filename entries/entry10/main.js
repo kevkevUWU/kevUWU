79ee162ee491b64091d2fb7ecb497ecd99696eb4
kevkevUWU <lik777@newschool.edu>
2024-12-03 18:25:37 -0500

    fixfix

--- entries/entry10/main.js
+++ entries/entry10/main.js
@@ -1,14 +1,38 @@
-console.log("hello!");
-
-
-
 
 let body = document.body;
+
 
 function turnOnLight() {
   body.classList.toggle("light");
   button.classList.toggle("buttonOn");
+  const newImage = document.createElement("img");
+  newImage.src = "https://mystickermania.com/cdn/stickers/cute-cats/pizza-cat-512x512.png";
+  newImage.alt = "Order Image";
+  newImage.style.position = "fixed";
+  newImage.style.top = "50%";
+  newImage.style.left = "50%";
+  newImage.style.transform = "translate(-50%, -50%)";
+  newImage.style.zIndex = "1000";
+  document.body.appendChild(newImage);
+  
 }
 
 let button = document.querySelector(".button");
 button.addEventListener("click", turnOnLight);
+
+document.addEventListener("wancheng", () => {
+  const mediaQuery = window.matchMedia("(max-width: 768px)");
+  if (mediaQuery.matches) {
+    document.querySelector(".hamburger").style.width = "90%";
+    document.querySelector(".hamburger").style.height = "auto";
+    document.querySelector("h1").style.fontSize = "32px";
+  }
+  if (orderButton) {
+    orderButton.style.position = "relative";
+    orderButton.style.top = "20px";
+    orderButton.style.marginTop = "20px";
+    orderButton.style.left = "20px"; 
+
+}
+});
+
--- entries/entry7/js/script.js
+++ entries/entry7/js/script.js
@@ -10,3 +10,49 @@ document.getElementById("locations").addEventListener("click", () => {
   hamburger.classList.toggle("open_top");
   hamburger.classList.remove("open_side");
 });
+
+document.querySelectorAll(".bun.top .seed").forEach(seed => {
+  seed.addEventListener("click", () => {
+    const seedName = seed.textContent.trim();
+    let url = "";
+    
+    switch(seedName) {
+      case "7th Street Burger":
+        url = "https://7thstreetburger.com";
+        break;
+      case "Burgers on B":
+        url = "https://burgersonb.com";
+        break;
+      case "Burger World":
+        url = "https://burgerworldnyc.com/lander";
+        break;
+      case "Burgermania":
+        url = "http://burgermanias.com";
+        break;
+      case "Smacking Burger":
+        url = "https://www.smackingburger.com";
+        break;
+      case "PLNT Burger":
+        url = "https://www.plntburger.com";
+        break;
+      case "Smash Burger":
+        url = "https://smashburger.com";
+        break;
+
+    }
+  
+  });
+  
+});
+document.querySelector(".menu_link.menu_1").addEventListener("click", () => {
+  window.location.href = "https://kevkevuwu.github.io/kevUWU/entries/entry7/js/index2.html";
+});
+
+document.querySelector(".menu_link.menu_2").addEventListener("click", () => {
+  window.location.href = "https://en.wikipedia.org/wiki/History_of_the_hamburger";
+});
+
+
+document.querySelector(".menu_link.menu_4").addEventListener("click", () => {
+  window.location.href = "https://kevkevuwu.github.io/kevUWU/entries/entry12/index.html";
+});
